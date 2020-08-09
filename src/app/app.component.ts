2e11b40d08969ede0073ce4ec27b3fde46c94115
eastcoastdeveloper <uxdeveloper@protonmail.com>
2020-08-09 08:49:27 -0400

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,13 @@
 import { Component, Injectable, VERSION, ViewChild, ElementRef } from '@angular/core';
 import { ProfilePanelService } from './services/profilePanel.service';
 
-function _window():any {
-  return window;
-}
-
-@Injectable()
-  export class WindowRef {
-    get nativeWindow():any {
-      return _window();
-    }
-  }
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ],
-  host: {
-    "(window:resize)":"onWindowResize($event)", "(window:scroll)":"onScroll($event)"
-  }
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
   
-  windowSize: any;
-  isMobile :boolean = false;
-  window:any;
-  width:number = window.innerWidth;
-  height:number = window.innerWidth;
-  mobileWidth:number = 760;
   modal:boolean;
   profilePanelStatus:boolean;
 
@@ -38,26 +18,7 @@ export class AppComponent  {
   ){}
 
   ngOnInit():void {
-    this.isMobile = this.width < this.mobileWidth;
     this.profileService.currentVal.subscribe(currentVal => this.profilePanelStatus = currentVal);
-  }
-
-  ngAfterViewInit(){
-    this.headerComponent.nativeElement.addEventListener('mouseover', function(){
-      this.classList.remove('header-scroll');
-    })
-  }
-
-  onWindowResize(event) {
-    this.width = event.target.innerWidth;
-    this.height = event.target.innerHeight;
-    this.isMobile = this.width < this.mobileWidth;
-  }
-
-  onScroll(event){
-    window.pageYOffset > 200 ?
-      this.headerComponent.nativeElement.classList.add('header-scroll') :
-      this.headerComponent.nativeElement.classList.remove('header-scroll')
   }
 
 }
